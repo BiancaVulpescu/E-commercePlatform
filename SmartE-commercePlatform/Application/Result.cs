e5a0c3676a88bfcb685b94f5be75fd608c92ad56
7h35up3rPr0 <skipperprivate@yahoo.com>
2024-11-11 22:33:37 +0200

    added

--- SmartE-commercePlatform/Application/Result.cs
+++ SmartE-commercePlatform/Application/Result.cs
@@ -20,6 +20,47 @@ public class Result<T, E>
         return new Result<T, E>(false, default, error);
     }
     public bool IsOk { get { return isOk; } }
+    public bool IsErr { get { return !isOk; } }
+    public bool IsOkAnd(Func<T, bool> predicate)
+    {
+        return isOk && predicate(value!);
+    }
+    public bool IsErrAnd(Func<E, bool> predicate)
+    {
+        return !isOk && predicate(error!);
+    }
+    public Result<U, E> Map<U>(Func<T, U> map)
+    {
+        return isOk ? new Result<U, E>(true, map(value!), default) : new Result<U, E>(false, default, error!);
+    }
+    public U MapOr<U>(U defaultValue, Func<T, U> mapSuccess)
+    {
+        return isOk ? mapSuccess(value!) : defaultValue;
+    }
+    public U MapOrElse<U>(Func<T, U> mapSuccess, Func<E, U> mapFailure)
+    {
+        return isOk ? mapSuccess(value!) : mapFailure(error!);
+    }
+    public Result<T, F> MapErr<F>(Func<E, F> mapErr)
+    {
+        return !isOk ? new Result<T, F>(false, default, mapErr(error!)) : new Result<T, F>(true, value!, default);
+    }
+    public Result<T, E> Inspect(Action<T> inspect)
+    {
+        if (isOk) 
+        {
+            inspect(value!);
+        }
+        return this;
+    }
+    public Result<T, E> InspectErr(Action<E> inspectErr)
+    {
+        if (!isOk)
+        {
+            inspectErr(error!);
+        }
+        return this;
+    }
     public T Unwrap()
     {
         if (!isOk)
@@ -28,6 +69,34 @@ public class Result<T, E>
         }
         return value!;
     }
+    public T? UnwrapOrDefault() 
+    {
+        return isOk ? value! : default;
+    }
+    public T? UnwrapOr(T? defaultValue)
+    {
+        return isOk ? value! : defaultValue;
+    }
+    public T? UnwrapOrElse(Func<E, T?> op)
+    {
+        return isOk ? value! : op(error!);
+    }
+    public Result<U, E> And<U>(Result<U, E> res)
+    {
+        return isOk ? res : new Result<U, E>(false, default, error!);
+    }
+    public Result<U, E> AndThen<U>(Func<T, Result<U, E>> op)
+    {
+        return isOk ? op(value!) : new Result<U, E>(false, default, error!);
+    }
+    public Result<T, F> Or<F>(Result<T, F> res)
+    {
+        return !isOk ? res : new Result<T, F>(true, value!, default);
+    }
+    public Result<T, F> OrElse<F>(Func<E, Result<T, F>> op)
+    {
+        return !isOk ? op(error!) : new Result<T, F>(true, value!, default);
+    }
     public E UnwrapErr()
     {
         if (isOk)
@@ -35,9 +104,5 @@ public class Result<T, E>
             throw new InvalidCastException("Result is not err!");
         }
         return error!;
-    }
-    public U MapOrElse<U>(Func<T, U> onSuccess, Func<E, U> onFailure)
-    {
-        return isOk ? onSuccess(value!) : onFailure(error!);
     }
 }
