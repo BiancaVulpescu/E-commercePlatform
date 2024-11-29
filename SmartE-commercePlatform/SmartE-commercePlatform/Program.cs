97eedf622798f00f65d18b9af2e19cd4516e911c
BiancaVulpescu <126167993+BiancaVulpescu@users.noreply.github.com>
2024-11-29 16:52:12 +0200

    policy modifications

--- SmartE-commercePlatform/SmartE-commercePlatform/Program.cs
+++ SmartE-commercePlatform/SmartE-commercePlatform/Program.cs
@@ -3,6 +3,17 @@ using Application;
 using Microsoft.EntityFrameworkCore.Query;
 
 var builder = WebApplication.CreateBuilder(args);
+var MyAllowSpecificOrigins = "MyAllowSpecificOrigins";
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: MyAllowSpecificOrigins,
+                      policy =>
+                      {
+                          policy.WithOrigins("http://localhost:4200");
+                          policy.AllowAnyHeader();
+                          policy.AllowAnyMethod();
+                      });
+});
 var dbPassword = Environment.GetEnvironmentVariable("DB_PASSWORD");
 var defaultConnection = builder.Configuration.GetConnectionString("DefaultConnection") + dbPassword;
 builder.Configuration["ConnectionStrings:DefaultConnection"] = defaultConnection;
@@ -24,6 +35,11 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseCors(MyAllowSpecificOrigins);
 
 app.UseHttpsRedirection();
 
