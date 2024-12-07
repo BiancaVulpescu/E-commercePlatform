f9d2225d9ae2754336bec16f9e29920af8805d45
daria-medeleanu <medeleanudaria14@gmail.com>
2024-12-07 20:56:00 +0200

    commit-trash

--- SmartE-commercePlatform/Identity/DependencyInjection.cs
+++ SmartE-commercePlatform/Identity/DependencyInjection.cs
@@ -1,12 +1,42 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using Domain.Repositories;
+using Identity.Repositories;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace Identity
 {
-    internal class DependencyInjection
+    public static class DependencyInjection
     {
+        public static IServiceCollection AddIdentity(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<UsersDbContext>(options => options.UseSqlite(configuration.GetConnectionString("UserConnection")));
+
+            // Add Authentication
+            var key = Encoding.ASCII.GetBytes("YourSecretKeyHere");
+            services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+            .AddJwtBearer(options =>
+            {
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuer = false,
+                    ValidateAudience = false,
+                    ValidateLifetime = true,
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(key)
+                };
+            });
+
+            services.AddScoped<IUserRepository, UserRepository>();
+
+            return services;
+        }
     }
 }
--- SmartE-commercePlatform/Identity/UserDbContext.cs
+++ SmartE-commercePlatform/Identity/UserDbContext.cs
@@ -1,7 +1,12 @@
-﻿namespace Identity
-{
-    public class UserDbContext
-    {
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
 
+public class UsersDbContext : DbContext
+{
+    public UsersDbContext(DbContextOptions<UsersDbContext> options) : base(options)
+    {
     }
+
+    public DbSet<User> Users { get; set; }
 }
