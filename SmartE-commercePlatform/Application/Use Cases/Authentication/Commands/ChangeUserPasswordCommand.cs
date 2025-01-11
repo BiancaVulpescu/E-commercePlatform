a7f5a91f90fc114c76fc55252a99d66c86a45916
Eusebiu Vatamaniuc <vatamaniuc.sebi@gmail.com>
2025-01-11 10:28:27 +0200

    Imlemented ChnageUserPassword  Feature

---
+++ SmartE-commercePlatform/Application/Use Cases/Authentication/Commands/ChangeUserPasswordCommand.cs
@@ -0,0 +1,12 @@
+﻿using ErrorOr;
+using MediatR;
+    
+namespace Application.Use_Cases.Authentication.Commands
+{
+    public class ChangeUserPasswordCommand : IRequest<ErrorOr<Success>>
+    {
+        public required Guid TokenId { get; set; }
+        public required string CurrentPassword { get; set; }
+        public required string NewPassword { get; set; }
+    }
+}
