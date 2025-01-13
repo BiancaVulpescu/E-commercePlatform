1ad742cd17971b72a6f5e604a51de64e207255a5
Eusebiu Vatamaniuc <vatamaniuc.sebi@gmail.com>
2025-01-13 17:47:07 +0200

    GetUserId method

---
+++ SmartE-commercePlatform/Application/Use Cases/QueryHandlers/User/GetUserIdQueryHandler.cs
@@ -0,0 +1,18 @@
+﻿using Application.Use_Cases.Authentication.Queries;
+using Domain.Entities;
+using Domain.Repositories;
+using ErrorOr;
+using MediatR;
+
+namespace Application.Use_Cases.Authentication.QueryHandlers
+{
+    public class GetUserIdQueryHandler(IUserRepository repository) : IRequestHandler<GetUserIdQuery, ErrorOr<Guid>>
+    {
+        private readonly IUserRepository repository = repository;
+
+        public async Task<ErrorOr<Guid>> Handle(GetUserIdQuery request, CancellationToken cancellationToken)
+        {
+            return await repository.GetUserId(request.TokenId, cancellationToken);
+        }
+    }
+}
